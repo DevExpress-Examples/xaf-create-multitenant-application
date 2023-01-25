0eda54a6724ecb5c3a427743cb72d8fc5596bf0b
87Alex <39731874+87Alex@users.noreply.github.com>
2023-01-25 14:49:06 +0300

    Add files via upload

--- SAASExample1.Module/DatabaseUpdate/Updater.cs
+++ SAASExample1.Module/DatabaseUpdate/Updater.cs
@@ -19,6 +19,21 @@ public class Updater : ModuleUpdater {
     public override void UpdateDatabaseAfterUpdateSchema() {
         base.UpdateDatabaseAfterUpdateSchema();
 #if !RELEASE
+        ApplicationUser sampleUser = ObjectSpace.FirstOrDefault<ApplicationUser>(u => u.UserName == "User");
+        if (sampleUser == null) {
+            sampleUser = ObjectSpace.CreateObject<ApplicationUser>();
+            sampleUser.UserName = "User";
+            // Set a password if the standard authentication type is used
+            sampleUser.SetPassword("");
+
+            // The UserLoginInfo object requires a user object Id (Oid).
+            // Commit the user object to the database before you create a UserLoginInfo object. This will correctly initialize the user key property.
+            ObjectSpace.CommitChanges(); //This line persists created object(s).
+            ((ISecurityUserWithLoginInfo)sampleUser).CreateUserLoginInfo(SecurityDefaults.PasswordAuthentication, ObjectSpace.GetKeyValueAsString(sampleUser));
+        }
+        PermissionPolicyRole defaultRole = CreateDefaultRole();
+        sampleUser.Roles.Add(defaultRole);
+
         ApplicationUser userAdmin = ObjectSpace.FirstOrDefault<ApplicationUser>(u => u.UserName == "Admin");
         if(userAdmin == null) {
             userAdmin = ObjectSpace.CreateObject<ApplicationUser>();
@@ -51,15 +66,20 @@ public class Updater : ModuleUpdater {
             defaultRole = ObjectSpace.CreateObject<PermissionPolicyRole>();
             defaultRole.Name = "Default";
 
+            defaultRole.PermissionPolicy = SecurityPermissionPolicy.AllowAllByDefault;
             defaultRole.AddObjectPermissionFromLambda<ApplicationUser>(SecurityOperations.Read, cm => cm.ID == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
             defaultRole.AddNavigationPermission(@"Application/NavigationItems/Items/Default/Items/MyDetails", SecurityPermissionState.Allow);
             defaultRole.AddMemberPermissionFromLambda<ApplicationUser>(SecurityOperations.Write, "ChangePasswordOnFirstLogon", cm => cm.ID == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
             defaultRole.AddMemberPermissionFromLambda<ApplicationUser>(SecurityOperations.Write, "StoredPassword", cm => cm.ID == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
+            defaultRole.AddTypePermissionsRecursively<ApplicationUser>(SecurityOperations.Read, SecurityPermissionState.Deny);
             defaultRole.AddTypePermissionsRecursively<PermissionPolicyRole>(SecurityOperations.Read, SecurityPermissionState.Deny);
             defaultRole.AddTypePermissionsRecursively<ModelDifference>(SecurityOperations.ReadWriteAccess, SecurityPermissionState.Allow);
             defaultRole.AddTypePermissionsRecursively<ModelDifferenceAspect>(SecurityOperations.ReadWriteAccess, SecurityPermissionState.Allow);
 			defaultRole.AddTypePermissionsRecursively<ModelDifference>(SecurityOperations.Create, SecurityPermissionState.Allow);
             defaultRole.AddTypePermissionsRecursively<ModelDifferenceAspect>(SecurityOperations.Create, SecurityPermissionState.Allow);
+            defaultRole.AddNavigationPermission(@"Application/NavigationItems/Items/Default/Items/ModelDifference_ListView", SecurityPermissionState.Deny);
+            defaultRole.AddNavigationPermission(@"Application/NavigationItems/Items/Default/Items/PermissionPolicyRole_ListView", SecurityPermissionState.Deny);
+            defaultRole.AddNavigationPermission(@"Application/NavigationItems/Items/Default/Items/ApplicationUser_ListView", SecurityPermissionState.Deny);
         }
         return defaultRole;
     }
