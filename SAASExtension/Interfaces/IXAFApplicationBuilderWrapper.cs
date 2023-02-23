285cc6d87a6854cbf0531420d94ad06fd51d4c05
87Alex <39731874+87Alex@users.noreply.github.com>
2023-02-23 10:40:10 +0300

    Add files via upload

---
+++ SAASExtension/Interfaces/IXAFApplicationBuilderWrapper.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.ExpressApp;
+
+namespace SAASExtension.Interfaces {
+    public interface IXAFApplicationBuilderWrapper {
+        IXAFApplicationBuilderWrapper ConfigureOptions<TOptions>(Action<TOptions> configureOptions) where TOptions : class;
+        IXAFApplicationBuilderWrapper AddService<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService;
+        IXAFApplicationBuilderWrapper AddLogonController<TLogonController>(Action<TLogonController> configure = null) where TLogonController : Controller, new();
+        IXAFApplicationBuilderWrapper AddBuildStep(Action<XafApplication> configureApplication);
+    }
+}
