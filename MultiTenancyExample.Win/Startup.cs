9114dc29c29b8e5778c9c7ddd71b290689ea8046
87Alex <39731874+87Alex@users.noreply.github.com>
2023-03-15 14:04:41 +0300

    Add files via upload

---
+++ MultiTenancyExample.Win/Startup.cs
@@ -0,0 +1,193 @@
+﻿using System.Configuration;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Win.ApplicationBuilder;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Win;
+using DevExpress.Persistent.Base;
+using Microsoft.EntityFrameworkCore;
+using DevExpress.ExpressApp.EFCore;
+using DevExpress.EntityFrameworkCore.Security;
+using DevExpress.XtraEditors;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.ExpressApp.Design;
+using Microsoft.Extensions.DependencyInjection;
+using MultiTenancyExample.Module;
+using MultiTenancyExtensionWin;
+using MultiTenancyExtension.Security;
+using MultiTenancyExtension.BusinessObjects;
+using MultiTenancyExtension.Services;
+using MultiTenancyExample.Module.BusinessObjects;
+using DevExpress.ExpressApp.MiddleTier;
+using MultiTenancyExtension;
+
+namespace MultiTenancyExample.Win;
+
+public class ApplicationBuilder : IDesignTimeApplicationFactory {
+    public static WinApplication BuildApplication(string connectionString) {
+        var builder = WinApplication.CreateBuilder();
+        builder.UseApplication<MultiTenancyExampleWindowsFormsApplication>();
+
+#if TenantFirst
+        builder
+            .AddMultiTenancyModelDifferenceStore(mds => {
+                mds.Assembly = typeof(MultiTenancyExampleModule).Assembly;
+            })
+            .MakeMultiTenancy(o => {
+                o.SelectTenantPropertyCaption = "Company";
+                o.TenantObjectDisplayName = "Company";
+                o.LogonFormCaption = "Log In";
+                o.RemoveExtraNavigationItems = true;
+            })
+            .MultipleDatabases(builder => {
+                ((IWinApplicationBuilder)builder).ObjectSpaceProviders.AddSecuredEFCore().WithDbContext<MultiTenancyExampleEFCoreDbContext>((application, options) => {
+                    options.UseDefaultSQLServerMultiTenancyOptions(application.ServiceProvider);
+                }, ServiceLifetime.Transient);
+            })
+            .TenantFirst<ApplicationUser>();
+            //.AddSelectTenantsLogonController();
+#endif
+#if TenantFirstOneDatabase
+        builder
+        .AddMultiTenancyModelDifferenceStore(mds => {
+            mds.Assembly = typeof(MultiTenancyExampleModule).Assembly;
+            mds.ServiceModelResourceName = "ServiceModelOneDatabase";
+        })
+        .MakeMultiTenancy(o => {
+            o.SelectTenantPropertyCaption = "Company";
+            o.TenantObjectDisplayName = "Company";
+            o.LogonFormCaption = "Log In";
+            o.RemoveExtraNavigationItems = true;
+        })
+        .OneDatabase()
+        .TenantFirst()
+        .AddSelectTenantsLogonController();
+        builder.ObjectSpaceProviders
+          .AddSecuredEFCore().WithDbContext<MultiTenancyExample.Module.BusinessObjects.MultiTenancyExampleEFCoreDbContext>((application, options) => {
+           options.UseDefaultSQLServerMultiTenancyOptions(application.ServiceProvider);
+        });
+#endif
+#if LogInFirst
+        builder
+        .AddMultiTenancyModelDifferenceStore(mds => {
+            mds.Assembly = typeof(MultiTenancyExampleModule).Assembly;
+            mds.ServiceModelResourceName = "ExtendedServiceModel";
+            mds.ProductionModelResourceName = "LiteProductionModel";
+        })
+        .MakeMultiTenancy(o => {
+            o.SelectTenantPropertyCaption = "Company";
+            o.SelectTenantFormCaption = "Select Company";
+            o.TenantObjectDisplayName = "Company";
+            o.LogonFormCaption = "Log In";
+            o.RemoveExtraNavigationItems = true;
+        })
+        .MultipleDatabases(builder => {
+            ((IWinApplicationBuilder)builder).ObjectSpaceProviders.AddSecuredEFCore().WithDbContext<MultiTenancyExampleEFCoreDbContext>((application, options) => {
+                options.UseDefaultSQLServerMultiTenancyOptions(application.ServiceProvider);
+            }, ServiceLifetime.Transient);
+        }).LogInFirst<ServiceDBContext<ApplicationUser, ApplicationUserLoginInfo>>()
+        .AddSelectUserTenantsLogonController();
+#endif
+        //#if LogInFirstOneDatabase
+        //        builder
+        //        .AddMultiTenancyModelDifferenceStore(mds => {
+        //            mds.Assembly = typeof(MultiTenancyExampleModule).Assembly;
+        //            mds.ServiceModelResourceName = "ExtendedServiceModel";
+        //            mds.ProductionModelResourceName = "LiteProductionModel";
+        //        })
+        //        .MakeMultiTenancy(o => {
+        //            o.SelectTenantPropertyCaption = "Company";
+        //            o.SelectTenantFormCaption = "Select Company";
+        //            o.TenantObjectDisplayName = "Company";
+        //            o.LogonFormCaption = "Log In";
+        //            o.RemoveExtraNavigationItems = true;
+        //        })
+        //        .OneDatabase()
+        //        .LogInFirst<ServiceDBContext<ApplicationUser, ApplicationUserLoginInfo>>()
+        //        .AddSelectTenantsRunTimeController()
+        //        //.AddSelectUserTenantsStartupAction();
+        //        .AddSelectUserTenantsLogonController();
+        //        builder.ObjectSpaceProviders
+        //            .AddSecuredEFCore().WithDbContext<MultiTenancyExampleEFCoreDbContext>((application, options) => {
+        //                options.UseDefaultSQLServerMultiTenancyOptions(application.ServiceProvider);
+        //            });
+        //#endif
+#if PredefinedTenant
+        builder
+        .AddMultiTenancyModelDifferenceStore(mds => {
+            mds.Assembly = typeof(MultiTenancyExampleModule).Assembly;
+            mds.ServiceModelResourceName = "PredefinedTenantServiceModel";
+            mds.ProductionModelResourceName = "LiteProductionModel";
+        })
+        .MakeMultiTenancy(o => {
+            o.TenantObjectDisplayName = "Company";
+            o.LogonFormCaption = "Log In";
+            o.RemoveExtraNavigationItems = true;
+        })
+        .MultipleDatabases(builder => {
+            ((IWinApplicationBuilder)builder).ObjectSpaceProviders.AddSecuredEFCore().WithDbContext<MultiTenancyExampleEFCoreDbContext>((application, options) => {
+                options.UseDefaultSQLServerMultiTenancyOptions(application.ServiceProvider);
+            });
+        })
+        .PredefinedTenant<ServiceDBContext<ApplicationUser, ApplicationUserLoginInfo>>();
+#endif
+#if PredefinedTenantOneDatabase
+        builder
+        .AddMultiTenancyModelDifferenceStore(mds => {
+            mds.Assembly = typeof(MultiTenancyExampleModule).Assembly;
+            mds.ServiceModelResourceName = "PredefinedTenantServiceModel";
+            mds.ProductionModelResourceName = "LiteProductionModel";
+        })
+        .MakeMultiTenancy(o => {
+            o.TenantObjectDisplayName = "Company";
+            o.LogonFormCaption = "Log In";
+            o.RemoveExtraNavigationItems = true;
+        })
+        .OneDatabase()
+        .PredefinedTenant<ServiceDBContext<ApplicationUser, ApplicationUserLoginInfo>>();
+        builder.ObjectSpaceProviders
+           .AddSecuredEFCore().WithDbContext<MultiTenancyExampleEFCoreDbContext>((application, options) => {
+               options.UseDefaultSQLServerOptions(application.ServiceProvider);
+           });
+#endif
+
+        builder.Modules
+            .AddConditionalAppearance()
+            .AddValidation(options => {
+                options.AllowValidationDetailsAccess = false;
+            })
+            .Add<MultiTenancyExampleModule>()
+        	.Add<MultiTenancyExampleWinModule>();
+        builder.ObjectSpaceProviders.AddNonPersistent();
+        builder.Security
+            .UseIntegratedMode(options => {
+                options.RoleType = typeof(PermissionPolicyRole);
+                options.UserType = typeof(MultiTenancyExample.Module.BusinessObjects.ApplicationUser);
+                options.UserLoginInfoType = typeof(MultiTenancyExample.Module.BusinessObjects.ApplicationUserLoginInfo);
+            })
+#if TenantFirst || LogInFirst || LogInFirstOneDatabase || PredefinedTenant || PredefinedTenantOneDatabase
+             .AddMultiTenancyPasswordAuthentication(options => {
+                 options.IsSupportChangePassword = true;
+             });
+#endif
+#if TenantFirstOneDatabase
+            .AddMultiTenancyPasswordAuthentication<CustomAuthenticationStandardProvider>(options => {
+                 options.IsSupportChangePassword = true;
+             });
+#endif
+
+        builder.AddBuildStep(application => {
+            //application.ConnectionString = connectionString;
+#if DEBUG
+            if(System.Diagnostics.Debugger.IsAttached && application.CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+                application.DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+            }
+#endif
+        });
+        var winApplication = builder.Build();
+        return winApplication;
+    }
+
+    XafApplication IDesignTimeApplicationFactory.Create()
+        => BuildApplication(XafApplication.DesignTimeConnectionString);
+}
---
+++ MultiTenancyExample.Win/WinApplication.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Win.Utils;
+
+using MultiTenancyExtensionWin;
+
+namespace MultiTenancyExample.Win;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Win.WinApplication._members
+public class MultiTenancyExampleWindowsFormsApplication : WinApplication {
+    public MultiTenancyExampleWindowsFormsApplication() {
+		SplashScreen = new DXSplashScreen(typeof(XafSplashScreen), new DefaultOverlayFormOptions());
+        ApplicationName = "MultiTenancyExample";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        UseOldTemplates = false;
+        DatabaseVersionMismatch += MultiTenancyExampleWindowsFormsApplication_DatabaseVersionMismatch;
+        CustomizeLanguagesList += MultiTenancyExampleWindowsFormsApplication_CustomizeLanguagesList;
+    }
+    private void MultiTenancyExampleWindowsFormsApplication_CustomizeLanguagesList(object sender, CustomizeLanguagesListEventArgs e) {
+        string userLanguageName = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+        if(userLanguageName != "en-US" && e.Languages.IndexOf(userLanguageName) == -1) {
+            e.Languages.Add(userLanguageName);
+        }
+    }
+    private void MultiTenancyExampleWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+			string message = "The application cannot connect to the specified database, " +
+				"because the database doesn't exist, its version is older " +
+				"than that of the application or its schema does not match " +
+				"the ORM data model structure. To avoid this error, use one " +
+				"of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+			if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+				message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+			}
+			throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
