10bac9629d1163fc5afee1f86345970eff639fc7
apobekiaris <apostolis.bekiaris@gmail.com>
2023-09-29 16:28:37 +0300

    card variant

---
+++ OutlookInspired.Blazor.Server/Components/Customers/CardView/Model.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OutlookInspired.Blazor.Server.Components.Customers.CardView{
+    public class Model:OutlookInspired.Blazor.Server.Components.Customers.Model{
+        protected override RenderFragment FragmentSelector(Customers.Model model) => CardView.Create((Model)model);
+    }
+}
