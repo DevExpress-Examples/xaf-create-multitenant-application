4efead04009749e6541d9e7f890b7c84c9d3dca4
apobekiaris <apostolis.bekiaris@gmail.com>
2023-10-30 19:12:36 +0200

    MapsTests

--- Tests/OutlookInspired.Blazor.Tests/MapsTests.cs
+++ Tests/OutlookInspired.Blazor.Tests/MapsTests.cs
@@ -9,31 +9,31 @@ namespace OutlookInspired.Blazor.Tests{
     [Order(30)]
     public class MapsTests:TestBase{
         [RetryTestCaseSource(nameof(EmployeeVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Employee(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertEmployeeMaps(view, viewVariant));
         }
 
         [RetryTestCaseSource(nameof(CustomerVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Customer(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertCustomerMaps(view, viewVariant));
         }
 
         [RetryTestCaseSource(nameof(ProductVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Product(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertProductMaps(view, viewVariant));
         }
 
         [RetryTestCaseSource(nameof(OrderVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Order(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertOrderMaps(view, viewVariant));
         }
 
         [RetryTestCaseSource(nameof(OpportunityVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Opportunity(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertOpportunityMaps(view, viewVariant));
         }
--- Tests/OutlookInspired.Win.Tests/MapsTests.cs
+++ Tests/OutlookInspired.Win.Tests/MapsTests.cs
@@ -9,31 +9,31 @@ namespace OutlookInspired.Win.Tests{
     [Order(20)]
     public class MapsTests:TestBase{
         [RetryTestCaseSource(nameof(EmployeeVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Employee(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertEmployeeMaps(view, viewVariant));
         }
         
         [RetryTestCaseSource(nameof(CustomerVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Customer(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertCustomerMaps(view, viewVariant)).FirstOrDefaultAsync();
         }
 
         [RetryTestCaseSource(nameof(ProductVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Product(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertProductMaps(view, viewVariant));
         }
 
         [RetryTestCaseSource(nameof(OrderVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Order(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertOrderMaps(view, viewVariant));
         }
 
         [RetryTestCaseSource(nameof(OpportunityVariants),MaxTries=MaxTries)]
-        //[Category(Tests)]
+        [Category(Tests)]
         public async Task Opportunity(string user,string view,string viewVariant){
             await StartTest(user, application => application.AssertOpportunityMaps(view, viewVariant));
         }
