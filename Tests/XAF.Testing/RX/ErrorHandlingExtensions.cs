aec3fc07cfed1072f4f6eee922f33cea32b86dec
apobekiaris <apostolis.bekiaris@gmail.com>
2023-10-30 09:10:52 +0200

    MapsTests

--- Tests/XAF.Testing/RX/ErrorHandlingExtensions.cs
+++ Tests/XAF.Testing/RX/ErrorHandlingExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reactive.Linq;
 
 namespace XAF.Testing.RX{
-    class ErrorHandlingExtensions{
+    public static class ErrorHandlingExtensions{
         public static IObservable<T> CompleteOnError<T>(this IObservable<T> source,Action<Exception> onError=null,Func<Exception,bool> match=null)
             => source.Catch<T,Exception>(exception => {
                 if (!(match?.Invoke(exception) ?? true)) return exception.Throw<T>();
