b3482f0f7bc2510d7e2e2a59d504e1b2babee7b6
apobekiaris <apostolis.bekiaris@gmail.com>
2023-12-07 00:00:54 +0200

    Opportunities center percentage text

--- CS/OutlookInspired.Blazor.Server/Components/DevExtreme/Pivot/DxPivotGrid.razor.js
+++ CS/OutlookInspired.Blazor.Server/Components/DevExtreme/Pivot/DxPivotGrid.razor.js
@@ -1,27 +1,33 @@
 function PivotCellProgressBar(model) {
     model.options.onCellPrepared = function (e) {
         if (e.area === 'data' && model.options.dataSource.dataFields[e.columnIndex].isProgressBar) {
+            const fullProgressBar = document.createElement('div');
+            fullProgressBar.style.width = '100%';
+            fullProgressBar.style.backgroundColor = 'transparent'; 
+            fullProgressBar.style.height = '20px';
+            fullProgressBar.style.position = 'relative';
             const progressBar = document.createElement('div');
             progressBar.className = 'progress-bar';
             const progress = Math.round(e.cell.value * 100);
+            progressBar.style.width = progress + '%';
+            progressBar.style.backgroundColor = '#8F4700';
+            progressBar.style.height = '100%'; 
             const progressLabel = document.createElement('span');
             progressLabel.className = 'progress-label';
             progressLabel.innerHTML = progress + '%';
-            progressBar.style.width = progress + '%';
-            progressBar.style.backgroundColor = '#8F4700';
-            progressBar.style.height = '20px';
-            progressBar.style.position = 'relative';
             progressLabel.style.position = 'absolute';
             progressLabel.style.left = '50%';
             progressLabel.style.top = '50%';
             progressLabel.style.transform = 'translate(-50%, -50%)';
             progressLabel.style.color = '#fff';
-            progressBar.appendChild(progressLabel);
+            fullProgressBar.appendChild(progressBar);
+            fullProgressBar.appendChild(progressLabel);
             e.cellElement.innerHTML = '';
-            e.cellElement.appendChild(progressBar);
+            e.cellElement.appendChild(fullProgressBar);
         }
     };
 }
+
 
 export async function SetPivotGridSource(dxPivot, model) {
     dxPivot.option("dataSource",model.options.dataSource)
