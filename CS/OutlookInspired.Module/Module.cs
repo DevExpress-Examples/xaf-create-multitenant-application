9e8ce75a6620f604c1ba47e4abec03d591081169
Maksim Karpenko (DevExpress) <maksim.karpenko@devexpress.com>
2024-06-03 10:41:27 +0300

    Fix build

--- CS/OutlookInspired.Module/Module.cs
+++ CS/OutlookInspired.Module/Module.cs
@@ -56,7 +56,7 @@ public sealed class OutlookInspiredModule : ModuleBase{
 		    typeof(CommunicationController),typeof(RoutePointController), typeof(WelcomeController),
 		    typeof(FollowUpController),typeof(InvoiceReportDocumentController),typeof(InvoiceController),typeof(PayController),typeof(RefundController),typeof(Features.Orders.ReportController),typeof(ShipmentDetailController),
 		    typeof(Features.Products.ReportController),
-		    typeof(MasterDetailController),typeof(SplitterPositionController),typeof(ViewFilterController)
+		    typeof(MasterDetailController),typeof(SplitterPositionController),typeof(ViewFilterController),
 			typeof(EvaluationListViewController)
 	    };
 
