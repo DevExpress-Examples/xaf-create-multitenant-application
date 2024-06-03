06bb333f2f3f1869ef1540d28afcba398f33c04f
apobekiaris <apostolis.bekiaris@gmail.com>
2024-06-03 18:58:31 +0300

    EvaluationListViewController not exists

--- CS/OutlookInspired.Module/Module.cs
+++ CS/OutlookInspired.Module/Module.cs
@@ -57,7 +57,6 @@ public sealed class OutlookInspiredModule : ModuleBase{
 		    typeof(FollowUpController),typeof(InvoiceReportDocumentController),typeof(InvoiceController),typeof(PayController),typeof(RefundController),typeof(Features.Orders.ReportController),typeof(ShipmentDetailController),
 		    typeof(Features.Products.ReportController),
 		    typeof(MasterDetailController),typeof(SplitterPositionController),typeof(ViewFilterController)
-			typeof(EvaluationListViewController)
 	    };
 
     public override void Setup(XafApplication application) {
